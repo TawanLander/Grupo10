0722e9bfc14c2f017b580cdc8730fe92e4971bc3
Leonardo Moura de Sousa <leonardomourass2008@gmail.com>
2026-09-04 21:09:20 -0300

    corrigindo rotas, src em arquivos e o app.js para executar corretamente

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("dotenv").config({ path: ".env" });
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+
 const PORTA_APP = process.env.APP_PORT;
 const HOST_APP = process.env.APP_HOST;
 
@@ -12,7 +13,8 @@ const indexRouter = require("./src/routes/index");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public/html")));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,18 @@
 var express = require("express");
+var path = require("path");
+
 var router = express.Router();
 
 router.get("/", function (req, res) {
-    res.render("index");
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+});
+
+router.get("/sobre", function (req, res) {
+    res.sendFile(path.join(__dirname, "../../public/html/sobre.html"));
+});
+
+router.get("/contato", function (req, res) {
+    res.sendFile(path.join(__dirname, "../../public/html/contato.html"));
 });
 
 module.exports = router;
